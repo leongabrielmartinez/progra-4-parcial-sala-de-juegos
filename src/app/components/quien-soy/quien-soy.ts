76c2618fce290ac355bb7f7a82e53ff63b71c6d4
RSS <gabrielmartinezdev@gmail.com>
2026-05-16 21:34:15 -0300

    Rutas y componentes iniciales

---
+++ src/app/components/quien-soy/quien-soy.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quien-soy',
+  imports: [],
+  templateUrl: './quien-soy.html',
+  styleUrl: './quien-soy.css',
+})
+export class QuienSoy {}
