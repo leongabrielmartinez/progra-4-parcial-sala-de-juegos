78641bfb0dfd90f17ace0d2b06123e413c269de2
RSS <gabrielmartinezdev@gmail.com>
2026-05-17 21:58:54 -0300

    [FIX] configuración de entorno y servicio limpia de credenciales

--- src/app/services/github.ts
+++ src/app/services/github.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GithubUser } from '../models/github-user';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,9 @@ export class GithubService {
   private http = inject(HttpClient);
   private apiUrl = 'https://api.github.com/users';
   
-  // ⚠️ REEMPLAZA CON TU TOKEN REAL
-  private githubToken = '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'; 
-
   getUserProfile(username: string): Observable<GithubUser> {
     const headers = new HttpHeaders({
-      'Authorization': `token ${this.githubToken}`
+      'Authorization': `token ${environment.githubToken}`
     });
 
     return this.http.get<GithubUser>(`${this.apiUrl}/${username}`, { headers });
